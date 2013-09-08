9cf1752eb8a2cdd70d0ee0f3f9200a78fbe11244
ColinW520 <colinhagan@gmail.com>
2013-09-08 17:06:13 +0000

    up to 9.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 # app/controllers/users_controller.rb
 
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
   
   def show
     @user = User.find(params[:id])
@@ -21,10 +23,36 @@ class UsersController < ApplicationController
     end
   end
   
-  private
-  
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  def edit
   end
 
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+  
+    # Before filters
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -6,7 +6,7 @@ describe "User pages" do
 
   subject { page }
 
-  #******** PROFILE PAGE TESTS ********************
+
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }
@@ -15,7 +15,7 @@ describe "User pages" do
     it { should have_title(user.name) }
   end
 
-  #******* SIGNUP PAGE TESTS **********************
+
   describe "signup" do
     
     before { visit signup_path }
@@ -27,6 +27,8 @@ describe "User pages" do
         expect { click_button submit }.not_to change(User, :count)
       end
     end
+    
+    
     
     describe "with valid information" do
       before do
@@ -53,6 +55,39 @@ describe "User pages" do
             before { click_link "Sign out" }
             it { should have_link('Sign in') }
           end
+        
+        describe "edit" do
+          let(:user) { FactoryGirl.create(:user) }
+          before { visit edit_user_path(user) }
+          before do
+            sign_in user
+            visit edit_user_path(user)
+          end
+          
+          describe "page" do
+            it { should have_content("Update your profile") }
+            it { should have_title("Edit user") }
+            it { should have_link('change', href: 'http://gravatar.com/emails') }
+          end
+          
+          describe "with invalid information" do
+            let(:new_name)  { "New Name" }
+            let(:new_email) { "new@example.com" }
+            before do
+              fill_in "Name",             with: new_name
+              fill_in "Email",            with: new_email
+              fill_in "Password",         with: user.password
+              fill_in "Confirm Password", with: user.password
+              click_button "Save changes"
+            end
+
+            it { should have_title(new_name) }
+            it { should have_selector('div.alert.alert-success') }
+            it { should have_link('Sign out', href: signout_path) }
+            specify { expect(user.reload.name).to  eq new_name }
+            specify { expect(user.reload.email).to eq new_email }
+          end
+        end
       end
     end
   end
